51130c4a3dfe15c20384967b4c6070be0161e32b
Thomas de Wulf <thomas.de.wulf@telenet.be>
2017-12-04 15:40:06 +0100

    Add app-routing-module, page not found and add debt to module

---
+++ frontend/src/app/app-routing/app-routing-module.ts
@@ -0,0 +1,26 @@
+//UIt commentaar halen wanneer auth geimplementeerd is
+//import { AuthGuardService } from './../user/auth-guard.service';
+import {NgModule} from '@angular/core';
+import{ RouterModule, Routes, PreloadAllModules} from '@angular/router';
+
+import {PageNotFoundComponent} from '../page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+    {
+path: 'debt',
+//canActivate: [ AuthGaurdService],
+loadChildren: '../debt/debt.module#DebtModule'
+    },
+    {path: '', redirectTo: 'debt/list', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
+    ],
+    declarations: [],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { DebtComponent } from './debt/debt.component';
 import { AddDebtComponent } from './add-debt/add-debt.component';
 import { DebtListComponent } from './debt-list/debt-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DebtComponent,
     AddDebtComponent,
-    DebtListComponent
+    DebtListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
