6b63966076db505669d3f504d906619cce0039ca
Thomas de Wulf <thomas.de.wulf@telenet.be>
2017-12-04 17:59:48 +0100

    UI navbar + toevoegen vergeten methode auth

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './user/authentication.service';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Schulden 152e FOS De Kangoeroes';
+export class AppComponent implements OnInit {
+  ngOnInit(){ }
+
+  constructor(private authService: AuthenticationService){ }
+
+  get currentUser(): Observable<string> {
+    return this.authService.user$;
+  }
+
 }
