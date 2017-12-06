bf6172fd0ae81c69532d87e836299e6720e9a6d5
Thomas de Wulf <thomas.de.wulf@telenet.be>
2017-12-06 20:21:18 +0100

    move favicon

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.SCHULDEN_DATABASE,{
 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
